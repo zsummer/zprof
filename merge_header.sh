1e06ce5f4536f05377916861dff9980ae0b8112b
zsummer <yawei.zhang@foxmail.com>
2021-02-15 15:02:26 +0800

    update

---
+++ merge_header.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export LANG=C
+export LC_CTYPE=C
+export LC_ALL=C
+
+cat src/zperf_counter.h  |sed `echo -e 's/\xEF\xBB\xBF//'` > src/zperf.h.bak
+cat src/zperf_record.h  |sed `echo -e 's/\xEF\xBB\xBF//'` >> src/zperf.h.bak
+cat src/zperf.h  |sed `echo -e 's/\xEF\xBB\xBF//'` >> src/zperf.h.bak
+
+cat src/zperf.h.bak | sed '/#include.*zperf_/d' > src/zperf.h.merge
+rm src/zperf.h.bak
