a4de6f2f7544bee10f9428022250d517fa2a83d1
zsummer <yawei.zhang@foxmail.com>
2021-06-14 03:31:35 +0800

    add lock instruction

--- src/zperf_record.h
+++ src/zperf_record.h
@@ -610,11 +610,12 @@ int PerfRecord<INST, RESERVE, DECLARE,  ANON>::init_perf(const char* desc)
     circles_per_ns_[PERF_COUNTER_CLOCK] = perf_get_time_inverse_frequency<PERF_COUNTER_CLOCK>();
     circles_per_ns_[PERF_CONNTER_CHRONO] = perf_get_time_inverse_frequency<PERF_CONNTER_CHRONO>();
     circles_per_ns_[PERF_COUNTER_RDTSC] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSC>();
-    circles_per_ns_[PERF_COUNTER_RDTSC_STOP] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSC_STOP>();
-    circles_per_ns_[PERF_COUNTER_RDTSCP] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSCP>();
-    circles_per_ns_[PERF_COUNTER_RDTSC_MFENCE] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSC_MFENCE>();
-    circles_per_ns_[PERF_COUNTER_RDTSC_NOFENCE] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSC_NOFENCE>();
-    circles_per_ns_[PERF_COUNTER_RDTSC_PURE] = perf_get_time_inverse_frequency<PERF_COUNTER_RDTSC_PURE>();
+    circles_per_ns_[PERF_COUNTER_RDTSC_STOP] = circles_per_ns_[PERF_COUNTER_RDTSC];
+    circles_per_ns_[PERF_COUNTER_RDTSCP] = circles_per_ns_[PERF_COUNTER_RDTSC];
+    circles_per_ns_[PERF_COUNTER_RDTSC_MFENCE] = circles_per_ns_[PERF_COUNTER_RDTSC];
+    circles_per_ns_[PERF_COUNTER_RDTSC_NOFENCE] = circles_per_ns_[PERF_COUNTER_RDTSC];
+    circles_per_ns_[PERF_COUNTER_RDTSC_PURE] = circles_per_ns_[PERF_COUNTER_RDTSC];
+    circles_per_ns_[PERF_COUNTER_RDTSC_LOCK] = circles_per_ns_[PERF_COUNTER_RDTSC];
     circles_per_ns_[PERF_COUNTER_NULL] = circles_per_ns_[PERF_COUNTER_DEFAULT];
 
     for (int i = node_begin_id(); i < node_reserve_end_id(); i++)
