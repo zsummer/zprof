894816ff2a87bbf958c33481e3662432290e483a
zsummer <yawei.zhang@foxmail.com>
2024-02-24 16:16:59 +0800

    change user output format

--- src/zprof_record.h
+++ src/zprof_record.h
@@ -1185,11 +1185,11 @@ namespace zprof
             if (true)
             {
                 rp.push_human_count(node.user.param1);
-                rp.push_string(STRLEN(", "));
+                rp.push_string(STRLEN(" / "));
                 rp.push_human_count(node.user.param2);
-                rp.push_string(STRLEN(", "));
+                rp.push_string(STRLEN(" / "));
                 rp.push_human_count(node.user.param3);
-                rp.push_string(STRLEN(", "));
+                rp.push_string(STRLEN(" / "));
                 rp.push_human_count(node.user.param4);
             }
 
